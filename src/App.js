a8bcae54fd535eda810ac3bf48ea3e171bcdc366
tjduigna <tjduigna@buffalo.edu>
2019-10-26 20:57:23 -0400

    A landing page

--- src/App.js
+++ src/App.js
@@ -2,24 +2,109 @@
 // Distributed under the terms of the Apache License 2.0
 
 import React from "react"
-import logo from "./logo.svg"
-// import "./App.css"
+// import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles'
 
-import Router from "./router.jsx"
+import Login from './login.jsx'
+
+// Rename Router to Socket or something 
+// to disambiguate with react-router duh
+// import Router from "./router.jsx"
 
 
-class App extends React.Component {
-
-  render() {
-    return (
+      /*
+import PersistentDrawerLeft from './drawer.jsx'
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Router />
         </header>
+        <PersistentDrawerLeft />
       </div>
+      */
+
+const useStyles = makeStyles(theme => ({
+    app: {
+        backgroundColor: '#686c74',
+        display: 'flex',
+        height: '100vh',
+        width: '100vw',
+        flexDirection: 'column',
+    },
+}))
+
+export default function App() {
+    const classes = useStyles()
+    const theme = useTheme()
+
+    return (
+        <div className={classes.app}>
+            <Login />
+        </div>
     )
-  }
 }
 
-export default App
+
+//import {
+//  BrowserRouter as Router,
+//  Switch,
+//  Route,
+//  Link
+//} from "react-router-dom";
+//
+//        //style={{ float: 'left' }}>
+//
+//export default function App() {
+//  return (
+//    <Router>
+//      <div style={{
+//          display: 'flex',
+//          flexDirection: 'row'
+//      }}>
+//        <div>
+//        <img src={logo} />
+//        <nav>
+//          <ul>
+//            <li>
+//              <Link to="/">Home</Link>
+//            </li>
+//            <li>
+//              <Link to="/about">About</Link>
+//            </li>
+//            <li>
+//              <Link to="/users">Users</Link>
+//            </li>
+//          </ul>
+//        </nav>
+//        </div>
+//
+//        {/* A <Switch> looks through its children <Route>s and
+//            renders the first one that matches the current URL. */}
+//        <div>
+//        <Switch>
+//          <Route path="/about">
+//            <About />
+//          </Route>
+//          <Route path="/users">
+//            <Users />
+//          </Route>
+//          <Route path="/">
+//            <Home />
+//          </Route>
+//        </Switch>
+//        </ div>
+//      </div>
+//    </Router>
+//  );
+//}
+//
+//function Home() {
+//  return <h2>Home</h2>;
+//}
+//
+//function About() {
+//  return <h2>About</h2>;
+//}
+//
+//function Users() {
+//  return <h2>Users</h2>;
+//}
---
+++ src/login.jsx
@@ -0,0 +1,84 @@
+// Copright (c) 2019 Greatery Development Team
+// Distributed under the terms of the Apache License 2.0
+
+import React from 'react'
+
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import logo from "./logo.svg"
+// import fetch from 'fetch'
+
+const useStyles = makeStyles(theme => ({
+    house: {
+        width: '40vmin',
+        margin: 'auto',
+    },
+    appLogo: {
+        // margin: 'auto',
+    },
+    input: {
+        display: 'flex',
+    },
+}))
+
+export default function Login(props) {
+    const classes = useStyles()
+    const theme = useTheme()
+    const [username, setUsername] = React.useState('')
+    const [password, setPassword] = React.useState('')
+
+    const updateUsername = (evt) => {
+        setUsername(evt.target.value)
+    }
+    const updatePassword = (evt) => {
+        setPassword(evt.target.value)
+    }
+
+    console.log(username, password)
+
+    const tryLogin = () => {
+        fetch('http://localhost:5000/login', {
+            method: 'POST',
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*",
+                "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS", //"POST, GET, OPTIONS",
+                "Access-Control-Request-Headers": "Origin, Content-Type, Accept",
+            },
+            credentials: "same-origin",
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            })
+        }).then(response => {
+            console.log(response)
+        }).catch(console.error)
+    }
+
+    return (
+        <div className={classes.house}>
+            <img src={logo} className={classes.appLogo} />
+            <TextField variant="outlined"
+                       margin="dense"
+                       label="Username"
+                       onChange={updateUsername}
+                       className={classes.input} />
+            <TextField variant="outlined"
+                       margin="dense"
+                       label="Password"
+                       type="password"
+                       onChange={updatePassword}
+                       className={classes.input} />
+            <Button variant="outlined"
+                    margin="dense"
+                    fullWidth={true}
+                    onClick={tryLogin}>
+                    Login
+                    </Button>
+        </div>
+    )
+
+}
