c6eb009caea7ce504f5deb62ab1753623d25f18c
tjduigna <tjduigna@buffalo.edu>
2019-10-27 21:35:07 -0400

    Removes aws

--- src/login.jsx
+++ src/login.jsx
@@ -7,14 +7,6 @@ import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import logo from "./logo.svg"
 
-import Amplify, { Auth } from 'aws-amplify'
-import awsconfig from './aws-exports.js'
-
-Amplify.configure(awsconfig)
-
-const cfg = Auth.configure()
-console.log(cfg)
-
 const useStyles = makeStyles(theme => ({
     house: {
         width: '40vmin',
@@ -44,10 +36,6 @@ export default function Login(props) {
 
 
     const tryLogin = () => {
-//        const user = Auth.signIn(username, password)
-//        console.log(user)
-//        setUser(user)
-//        return user
         fetch('http://localhost:5000/login', {
             method: 'POST',
             headers: {
